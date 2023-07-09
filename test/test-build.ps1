ad76bd1c21d3ab0a0804bab313ae003cee255505
Alex Reinking <alex.reinking@gmail.com>
2023-07-09 16:25:21 -0400

    Override static lib naming on DLL systems

---
+++ test/test-build.ps1
@@ -0,0 +1,42 @@
+$ErrorActionPreference="Stop"
+
+$env:CMAKE_GENERATOR="Ninja"
+$env:CMAKE_BUILD_TYPE="Release"
+$env:CMAKE_TOOLCHAIN_FILE=Resolve-Path .\vcpkg\scripts\buildsystems\vcpkg.cmake
+
+# Clean up
+
+if (test-path build) { rm -r -fo build }
+if (test-path _local) { rm -r -fo _local }
+
+# Library builds
+
+cmake -S . -B build/shared -Ddiffuse_BUILD_APPS=NO -DBUILD_SHARED_LIBS=YES
+cmake -S . -B build/static -Ddiffuse_BUILD_APPS=NO -DBUILD_SHARED_LIBS=NO
+
+cmake --build build/shared
+cmake --build build/static
+
+cmake --install build/shared --prefix _local/shared
+cmake --install build/shared --prefix _local/both
+
+cmake --install build/static --prefix _local/static
+cmake --install build/static --prefix _local/both
+
+# App builds
+
+$env:CMAKE_PREFIX_PATH=Resolve-Path .\_local\shared
+cmake -S apps/sdl -B build/apps-shared
+cmake --build build/apps-shared
+
+$env:CMAKE_PREFIX_PATH=Resolve-Path .\_local\static
+cmake -S apps/sdl -B build/apps-static
+cmake --build build/apps-static
+
+$env:CMAKE_PREFIX_PATH=Resolve-Path .\_local\both
+cmake -S apps/sdl -B build/apps-both-static -Ddiffuse_SHARED_LIBS=NO
+cmake --build build/apps-both-static
+
+$env:CMAKE_PREFIX_PATH=Resolve-Path .\_local\both
+cmake -S apps/sdl -B build/apps-both-shared -Ddiffuse_SHARED_LIBS=YES
+cmake --build build/apps-both-shared
